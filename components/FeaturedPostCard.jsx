f5c11d9b5b4c27df4801979a4218eb4adace451b
emre-serdar <emreserdaar@gmail.com>
2022-12-03 14:26:36 -0500

    Styling applied for mobile nav bar

--- components/FeaturedPostCard.jsx
+++ components/FeaturedPostCard.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const FeaturedPostCard = ({ post }) => (
-  <div className="relative h-72">
+  <div className="relative h-72 ">
     <div className="absolute rounded-lg bg-center bg-no-repeat bg-cover shadow-md inline-block w-full h-72" style={{ backgroundImage: `url('${post.featuredImage.url}')` }} />
     <div className="absolute rounded-lg bg-center bg-gradient-to-b opacity-50 from-gray-400 via-gray-700 to-black w-full h-72" />
     <div className="flex flex-col rounded-lg p-4 items-center justify-center absolute w-full h-full">
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react'
 import { getCategories } from '../services';
 import Link from 'next/link'
-import { FiMenu } from 'react-icons/fi'
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
 
 
 const Header = () => {
@@ -15,8 +15,10 @@ const Header = () => {
       });
     }, []);  
 
-    
-
+    const [nav, setNav] = useState(false)
+    const handleNav = () => {
+        setNav(!nav)
+    }
   return (
 
     <div className='container mx-auto px-10 mb-8'>
@@ -29,9 +31,8 @@ const Header = () => {
                     </p>
                 </Link>
             </div>
-            <FiMenu className='hamburger lg:hidden float-right text-white h-10 w-10 cursor-pointer' onClick={() => setOpen(!open)} />
-            <nav className={`${open ? "block" : "hidden"} w-full lg:float-right lg:contents  `}>
-                
+           
+            <div className='hidden md:flex w-full lg:float-right lg:contents '>   
                 <ul className=''>
                     {categories.map((category, index)=> (
                         <li>
@@ -44,8 +45,31 @@ const Header = () => {
                         
                     ))}
                 </ul>        
-            </nav>
-            
+            </div>
+
+            {/* Hamburger menu */}
+            <div onClick={handleNav} className='block md:hidden'>
+                {nav ? <AiOutlineClose size={30} className=' text-white' /> : <AiOutlineMenu size={30} className='text-white' />}
+            </div>
+
+             {/* Mobile menu */}
+            <div className= {nav ? ' w-full nav  text-white absolute  mt-3  left-0 flex justify-center text-center z-50' : 'absolute left-[-100%]'}>
+                <ul>
+                    {categories.map((category, index)=> (
+                            <li className='inline'>
+                                <Link className=' '  key={index} href={`/category/${category.slug}`} >
+                                    <span className='md:float-right mt-2 align-middle text-sky-100 ml-4 mb-16 font-semibold cursor-pointer'>
+                                        {category.name}
+                                    </span>
+                                </Link>
+                            </li>
+                            
+                        ))}
+                </ul>
+            </div>
+                
+
+
         </div>
                   
     </div>
